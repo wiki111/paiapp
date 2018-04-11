aff8d11dd4564eaba7cb66a404443a742bc93848
MaciejWikira <maciej.wiqira@gmail.com>
2018-04-11 15:22:48 +0200

    Started refactoring looks with bootstrap

--- src/main/java/wikira/paiapp/configs/WebSecurityConfig.java
+++ src/main/java/wikira/paiapp/configs/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/home", "/", "/user/registration*", "/user/successRegister*", "/user/login", "/h2-console/**").permitAll()
+                .antMatchers("/home", "/", "/user/registration*", "/user/successRegister*", "/user/login", "/h2-console/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
